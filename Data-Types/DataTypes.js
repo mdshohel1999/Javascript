a2e09ef1ffec65a8fbc9bd1601d4095cad29c6d9
shohel <mdshohel011193002@gmail.com>
2023-08-30 12:12:23 +0600

    Javascript Data types

---
+++ Data-Types/DataTypes.js
@@ -0,0 +1,21 @@
+//* String : 
+const name = 'Hi, This is shohel';
+console.log(name);
+
+//? Number : 
+const number = 20;
+console.log(number)
+
+//! Undefined
+// const x = 
+// console.log(x);
+
+//* Bigint
+
+const a = 120;
+console.log(a);
+
+//? boolean
+
+const bb = true;
+console.log(bb);
--- Variable/Variable.js
+++ Variable/Variable.js
@@ -2,3 +2,25 @@ const firstNumber = 10;
 const secondNumber = 20;
 const result = firstNumber + secondNumber;
 console.log('The Two number summation is : ', result);
+
+
+//? Let 
+
+let firstNum = 10;
+let secondNum = 30;
+console.log('first Number + second Number  : ', firstNum + secondNum);
+
+//! upadate first number 
+firstNum = 50;
+console.log('Update first Number + second Number  : ', firstNum + secondNum);
+
+//* Var
+
+var a = 10;
+var b = 20;
+console.log(a + b);
+
+// Change value
+
+a = 50;
+console.log('After : ',a + b);
